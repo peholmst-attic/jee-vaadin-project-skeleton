8e732fd4f0d4c26420c9f032f301f252ce033a05
Petter Holmström <petter@vaadin.com>
2015-09-24 08:47:28 +0300

    Added DeltaSpike

--- skeleton/skeleton-ui/src/main/java/com/vaadin/jee/skeleton/control/SampleRepository.java
+++ skeleton/skeleton-ui/src/main/java/com/vaadin/jee/skeleton/control/SampleRepository.java
@@ -1,22 +1,12 @@
 package com.vaadin.jee.skeleton.control;
 
-import com.vaadin.jee.skeleton.control.common.BaseRepository;
 import com.vaadin.jee.skeleton.entity.SampleEntity;
+import org.apache.deltaspike.data.api.EntityRepository;
+import org.apache.deltaspike.data.api.Repository;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Transactional(Transactional.TxType.MANDATORY)
-public class SampleRepository extends BaseRepository<SampleEntity> {
-
-    @Inject
-    public SampleRepository(EntityManager entityManager) {
-        super(entityManager);
-    }
-
-    @Override
-    protected Class<SampleEntity> getEntityClass() {
-        return SampleEntity.class;
-    }
+@Repository(forEntity = SampleEntity.class)
+public interface SampleRepository extends EntityRepository<SampleEntity, Long> {
 }
