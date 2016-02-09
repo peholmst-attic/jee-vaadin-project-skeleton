eec6578ff3acbac23ffa8c86d48c8c79b55193c6
Petter Holmström <petter@vaadin.com>
2016-02-09 10:35:30 +0200

    Updated versions, removed @Transactional from SampleRepository, which was causing problems with WELD and Wildfly 10.

--- archetype/src/main/resources/archetype-resources/__rootArtifactId__-ui/src/main/java/control/SampleRepository.java
+++ archetype/src/main/resources/archetype-resources/__rootArtifactId__-ui/src/main/java/control/SampleRepository.java
@@ -8,9 +8,6 @@ import ${package}.entity.SampleEntity;
 import org.apache.deltaspike.data.api.EntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
-import javax.transaction.Transactional;
-
-@Transactional(Transactional.TxType.MANDATORY)
 @Repository(forEntity = SampleEntity.class)
 public interface SampleRepository extends EntityRepository<SampleEntity, Long> {
 }
--- skeleton/skeleton-ui/src/main/java/com/vaadin/jee/skeleton/control/SampleRepository.java
+++ skeleton/skeleton-ui/src/main/java/com/vaadin/jee/skeleton/control/SampleRepository.java
@@ -4,9 +4,6 @@ import com.vaadin.jee.skeleton.entity.SampleEntity;
 import org.apache.deltaspike.data.api.EntityRepository;
 import org.apache.deltaspike.data.api.Repository;
 
-import javax.transaction.Transactional;
-
-@Transactional(Transactional.TxType.MANDATORY)
 @Repository(forEntity = SampleEntity.class)
 public interface SampleRepository extends EntityRepository<SampleEntity, Long> {
 }
